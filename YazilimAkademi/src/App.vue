20fc90dbed20c3d7e213645ffa3bda8c6b556e99
Kadir Çetin <kadirrcetin@gmail.com>
2024-03-05 01:34:19 +0300

    YazilimAkademi-1

---
+++ YazilimAkademi/src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="header">
+
+<div class="container mt-2">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <img src="/src/assets/img/logo.png" style="height: 36px;" alt="idealwork logo" class="me-3">
+   
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item me-2">
+          <a class="nav-link active" aria-current="page" href="#">Kurslar</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Video Galeri</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Hakkımızda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Bize Ulaşın</a>
+        </li>
+       
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</div>
+
+</div>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+
+.header{
+  background-color: #fff;
+}
+</style>
